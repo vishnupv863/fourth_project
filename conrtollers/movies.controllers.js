a9e090d7dd7555d0da30d31565073f5648b18cef
vishnupv863 <vishnupv863@gmail.com>
2025-01-05 23:44:52 +0530

    singleMovie from movies.controllers updated

--- conrtollers/movies.controllers.js
+++ conrtollers/movies.controllers.js
@@ -2,10 +2,26 @@ import Movie from '../models/movies.models.js'
 
 
 // for getting details of all movie lists
-export const movieIndex = async (req, res) => { }
+export const movieIndex = async (req, res) => {
+    try {
+        const movieList = await Movie.find();
+        return res.status(200).json(movieList);
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+}
 
 // for getting details of a single movie
-export const singleMovie = async (req, res) => { }
+export const singleMovie = async (req, res) => {
+    try {
+        const movieList = await Movie.findById({ _id: req.params.id });
+        return res.status(200).json(movieList);
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+}
 
 // for creating movie list
 export const movieCreate = async (req, res) => {
@@ -14,6 +30,7 @@ export const movieCreate = async (req, res) => {
             title: req.body.title,
             description: req.body.description
         })
+        movie.save();
 
         return res.status(200).json(movie)
 
